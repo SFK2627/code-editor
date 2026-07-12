bd8cce27730721c72050bdf7f9db66709206a7a5
SFK <109145884+SFK2627@users.noreply.github.com>
2026-07-12 19:45:20 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = 'mcs-coding-app-20260712-user-step85-emergency-stable';
+const CACHE_NAME = 'mcs-coding-app-20260712-user-step86-safe-full-editor-tools';
 const APP_SHELL = [
   './',
   './index.html',
   './index.html?fresh=1',
   './style.css',
-  './style.css?v=20260712-step85-emergency-stable',
+  './style.css?v=20260712-step86-safe-full-editor-tools',
   './script.js',
-  './script.js?v=20260712-step85-emergency-stable',
+  './script.js?v=20260712-step86-safe-full-editor-tools',
   './firebase-config.js',
   './manifest.webmanifest',
   './favicon.png',
