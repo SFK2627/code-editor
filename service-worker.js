6f69adad5b6b53db8b892a91cc7162fc370e0f68
SFK <109145884+SFK2627@users.noreply.github.com>
2026-07-28 22:32:51 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = 'ict-8-connect-step238-read-reduction';
+const CACHE_NAME = 'ict-8-connect-step237-recovery-on-working-share-join';
 const APP_SHELL = [
   './',
   './index.html',
-  './index.html?v=20260727-step238-read-reduction',
-  './index.html?fresh=step238-read-reduction',
+  './index.html?v=20260727-step237-recovery-on-working-share-join',
+  './index.html?fresh=step237-recovery-on-working-share-join',
   './style.css',
-  './style.css?v=20260727-step238-read-reduction',
+  './style.css?v=20260727-step237-recovery-on-working-share-join',
   './script.js',
-  './script.js?v=20260727-step238-read-reduction',
+  './script.js?v=20260727-step237-recovery-on-working-share-join',
   './firebase-config.js',
   './manifest.webmanifest',
   './favicon.png',
