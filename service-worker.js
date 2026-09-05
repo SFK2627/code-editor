272842655b185b274168a952aeae26fc8e9fd6f2
SFK <109145884+SFK2627@users.noreply.github.com>
2026-09-05 20:51:19 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ict8-connect-v300-drive-image-fix';
+const CACHE_NAME = 'ict8-connect-v301-hide-activities-hero-mobile';
 
 self.addEventListener('install', event => {
   event.waitUntil(self.skipWaiting());
