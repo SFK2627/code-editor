48160ef09e644602d291cf1637aaeb336a43bc3a
SFK <109145884+SFK2627@users.noreply.github.com>
2026-09-05 21:12:37 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ict8-connect-v302-compact-activity-viewer';
+const CACHE_NAME = 'ict8-connect-v304-instructions-info-icon';
 
 self.addEventListener('install', event => {
   event.waitUntil(self.skipWaiting());
